57d55f01de1c65d92b95a934b456d38f6ba96cbe
Yusniel Brocard <ybrocard@jobomas.com>
2016-06-26 21:42:24 -0500

    add updates

--- models.py
+++ models.py
@@ -7,4 +7,9 @@ class res_partner(models.Model):
 
     _inherit = 'res.partner'
 
-    nro_customer = fields.Char(string='Nro', size=10)
+    nro_supplier = fields.Char(string='N°', size=10)
+    nro_phone = fields.Char(string='Teléfono', size=15)
+    nro_phone_1 = fields.Char(string='Teléfono', size=15)
+    nro_mobile = fields.Char(string='Celular', size=15)
+
+    plazo = fields.Integer(string='Plazo', size=10)
